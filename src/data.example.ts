e36b369aca3151a9de02ea2761adf9aaff529450
younesfdj <ferradjiyounes69@gmail.com>
2024-03-10 01:47:27 +0100

    feat: added data.example.ts

---
+++ src/data.example.ts
@@ -0,0 +1,46 @@
+export const products = [
+  {
+    title: "Project1",
+    link: "https://project.com",
+    thumbnail: project1,
+  },
+];
+
+export const navitems = [
+  {
+    link: "https://www.linkdin.com",
+    icon: linkdin,
+    alt: "linkdin",
+  },
+  {
+    link: "https://www.github.com",
+    icon: github,
+    alt: "github",
+  },
+  {
+    link: "https://www.twitter.com",
+    icon: twitter,
+    alt: "twitter",
+  },
+];
+
+export const workData = [
+  {
+    logo: exampleWork,
+    link: "https://www.example.com",
+    post: "Example post",
+    company: "Example company",
+    responsabilites: ["Example resp1", "Example resp2"],
+  },
+];
+
+export const technologies = ["example1", "example2", "example3"];
+
+export const communityData = [
+  {
+    title: "Example title",
+    date: "Example date",
+    description: "Exampple community",
+    img: ExampleCommunity,
+  },
+];
