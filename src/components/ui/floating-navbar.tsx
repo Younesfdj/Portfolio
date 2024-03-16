0fadb30135234d5e2b2f24c6a547f1f4ce95b785
younesfdj <ferradjiyounes69@gmail.com>
2024-03-16 01:00:28 +0100

    fix: fixed FloatingNav responsiveness

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -26,7 +26,6 @@ export const FloatingNav = ({
     const [visible, setVisible] = useState(true);
 
     useMotionValueEvent(scrollYProgress, "change", (current) => {
-        // Check if current is not undefined and is a number
         if (typeof current === "number") {
             let direction = current! - scrollYProgress.getPrevious()!;
 
@@ -57,7 +56,7 @@ export const FloatingNav = ({
                     duration: 0.2,
                 }}
                 className={cn(
-                    "flex max-w-fit fixed flex-col top-1/3 border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] items-center justify-center ml-6",
+                    "flex max-w-fit fixed top-10  inset-x-0 mx-auto lg:mx-0 lg:flex-col lg:top-1/3 border border-white/[0.2] rounded-full bg-black shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] items-center justify-center lg:ml-6",
                     className
                 )}
             >
@@ -66,13 +65,13 @@ export const FloatingNav = ({
                         key={`link=${idx}`}
                         href={navItem.link}
                         className={cn(
-                            "relative text-neutral-50 items-center flex hover:text-neutral-300  p-5"
+                            "relative text-neutral-50 items-center flex hover:text-neutral-300 p-5"
                         )}
                     >
                         <Image alt={navItem.alt} className="" src={navItem.icon} />
                     </Link>
                 ))}
             </motion.div>
-        </AnimatePresence>
+        </AnimatePresence >
     );
 };
